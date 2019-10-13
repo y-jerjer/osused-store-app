b472e3009d856605bad918c7773137c7c10f6204
Ingyu Woo <iglenwoo@gmail.com>
2019-10-12 21:58:52 -0700

    fix import

--- src/pages/ItemPost/ItemPost.test.js
+++ src/pages/ItemPost/ItemPost.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import ItemPost from './index'
+import { ItemPost } from './index'
 
 it('renders without crashing', () => {
   shallow(<ItemPost />)
